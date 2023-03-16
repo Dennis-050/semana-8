c8e504b86ab6c09632180c67589d3e97654f6f39
Dennis-050 <gm0515032022@unab.edu.sv>
2023-03-16 13:34:19 -0600

    semana-8

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using pro8.Models;
+
+AlmacenContext db = new AlmacenContext();
+
+Console.WriteLine("Ingrese el nombre del producto: ");
+string nombre = Console.ReadLine();
+Console.WriteLine("Ingrese la descripción del producto: ");
+string descripcion = Console.ReadLine();
+Console.WriteLine("Ingrese el precio del producto: ");
+decimal precio = decimal.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese el stock del producto: ");
+int stock = int.Parse(Console.ReadLine());
+
+
+var producto = new Producto { Nombre = nombre, Descripcion = descripcion, Precio = precio, Stock = stock };
+db.Productos.Add(producto);
+db.SaveChanges();
